0ed51eddfa6c65f3adb29f0de40501dabc13f382
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-09 12:07:01 +0200

    [Params, Query, Body] - worked with params, query to send the request with additional information and Body to specify the body to be sent to the endpoint

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { query } from 'express';
+
+@Controller('users')
+export class UsersController {
+  @Get('/:id/:optional?')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
+    return 'You sent a get request to users endpoint';
+  }
+  @Post()
+  public createUsers(@Body() request: any) {
+    console.log(request);
+    return 'You sent a post request to users endpoint';
+  }
+}
