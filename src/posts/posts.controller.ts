e594cb272d9651679a26f44bdce8920656936108
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-16 14:49:51 +0200

    [Refactor] - refactored the enums and put them in their folder

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -11,4 +19,7 @@ export class PostsController {
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
   }
+
+  @Post()
+  public createPost(@Body(new ValidationPipe()) createPostDto: CreatePostDto) {}
 }
