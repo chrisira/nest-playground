2f50b28a0c9b37f4a16ca5cbf6032ddb4111f729
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-16 11:56:50 +0200

    [Documentation] - added the configuration to swagger and added apitags to group the related requests

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get('/:userId?')
+  @ApiTags('Posts')
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
   }
