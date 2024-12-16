50d031e844ff98fc28947a4cf184c0ce470935c7
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-16 10:37:48 +0200

    [Swagger] - added the swagger installation and configuration to the app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,9 +9,17 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform:true
+      transform: true,
     }),
   );
+  /*
+  swagger configuration
+
+  */
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  // Instantiate Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
