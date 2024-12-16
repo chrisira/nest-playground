9886fa86a263185db1ca53bdfdc45e07954b8db4
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-16 10:02:07 +0200

    [Circular dependency] - added the circular dependency between user service and auth service

---
+++ src/auth/providers/auth.service.ts
@@ -0,0 +1,23 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UserService } from 'src/users/providers/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UserService))
+    private readonly usersService: UserService,
+  ) {}
+  public login(email: string, password: string, id: string) {
+    //check user exists in in the database
+    const user = this.usersService.findOneById('1234');
+
+    //login
+
+    //return token
+    return 'SAMPLE TOKEN';
+  }
+
+  public isAuth() {
+    return true;
+  }
+}
