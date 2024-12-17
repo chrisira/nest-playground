bf086c4c69bd8edd1c105d612fd4db48307a959a
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-17 15:31:04 +0200

    [Repo] - creating a repository and simple business logic to save data into the database

--- src/users/providers/user.service.ts
+++ src/users/providers/user.service.ts
@@ -1,6 +1,10 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 /**
  * Class to connect to users table and perform business operations
@@ -10,6 +14,9 @@ export class UserService {
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
+
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   /**
@@ -41,5 +48,18 @@ export class UserService {
       firstName: 'Alice',
       email: 'alice@test.com',
     };
+  }
+
+  public async createUser(createUserDto: CreateUserDto) {
+    // check if user already exists with the same email
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+    // Handle exception
+    // Create a new user
+
+    let newUser = this.usersRepository.create(createUserDto);
+    newUser = await this.usersRepository.save(newUser);
+    return newUser;
   }
 }
