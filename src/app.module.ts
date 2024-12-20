c48cdbc0de7827a3f4178b6c7250a9ce6b836bd3
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-20 15:56:57 +0200

    [meta option module and entitu] - added teh meta option module and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Post } from './posts/post.entity';
 import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
+import { MetaOptionController } from './meta-option/meta-option.controller';
 
 @Module({
   imports: [
@@ -29,8 +31,9 @@ import { TagsModule } from './tags/tags.module';
       }),
     }),
     TagsModule,
+    MetaOptionsModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, MetaOptionController],
   providers: [AppService],
 })
 export class AppModule {}
