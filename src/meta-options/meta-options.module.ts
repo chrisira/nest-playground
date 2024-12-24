6641bca99e837d9b016226a82bcbc106d0f95cea
Christian IRADUKUNDA <christianiradukunda6@gmail.com>
2024-12-24 21:40:43 +0200

    [Service] - added the meta options service start

--- src/meta-options/meta-options.module.ts
+++ src/meta-options/meta-options.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MetaOption } from './meta-option.entity';
+import { MetaOptionsService } from './providers/meta-options.service';
 
 @Module({
 
     imports: [TypeOrmModule.forFeature([MetaOption])],
+
+    providers: [MetaOptionsService],
 })
 export class MetaOptionsModule {}
